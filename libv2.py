3970586a14d09b806854a323f852e68197b27036
Tanish200-ind <276216719+Tanish200-ind@users.noreply.github.com>
2026-06-23 22:18:11 +0530

    update

--- libv2.py
+++ libv2.py
@@ -3,7 +3,7 @@ import os
 
 
 class Book:
-    def __init__(self, title, author, available="available"):
+    def __init__(self, title, author, available):
         self.title = title
         self.author = author
         self.available = available
